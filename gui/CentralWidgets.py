87015b938cd687272393844734125d182c58e86a
Mauricio <maurio.aravena@sansano.usm.cl>
2022-07-25 13:30:46 -0400

    Protoybe of abort sequence
    Abort sequence is working, but it needs some cleaning

--- gui/CentralWidgets.py
+++ gui/CentralWidgets.py
@@ -9,6 +9,10 @@ __version__ ='0.1'
 __author__ = 'maurio.aravena@sansano.usm.cl'
 
 
+# Constants
+ABORT_CMD = 'az.{channel}P1=0.000\r'
+
+
 class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -16,6 +20,9 @@ class CentralWidget(QWidget):
         #Objects 
         self.Thread = None
         self.Dispatcher = None
+        self.dispatcher_ctrl = None
+        self.route = None
+        self.comms = None
 
         # Widgets
         # ->Connection Widget
@@ -63,14 +70,18 @@ class CentralWidget(QWidget):
 
         # Create a Dispatcher
         # -> Get serial_comms 
-        comms = self.connection_wdg.getComms()
+        self.comms = self.connection_wdg.getComms()
         
         # -> Get Channel
         channel = int(self.channels_wdg.getChannel())*2
 
         # -> Get Route 
-        route = self.control_wdg.getRoute()
-        self.Dispatcher = Dispatcher(comms, channel, route)
+        self.route = self.control_wdg.getRoute()
+
+        # -> Generate dispatcher ctrl
+        self.dispatcher_ctrl = {'allowed':True}
+
+        self.Dispatcher = Dispatcher(self.comms, channel, self.route, self.dispatcher_ctrl)
 
         # Assign worker to thread
         self.Dispatcher.moveToThread(self.Thread)
@@ -89,8 +100,28 @@ class CentralWidget(QWidget):
 
         # Stop any running threads
         if self.Thread and self.Thread.isRunning():
-            print("CRISIS AVOIDED")
             
+            # Disable thread ability to send cmds to device
+            self.dispatcher_ctrl['allowed'] = False
+            
+            # Set thread timer to zero
+            self.route['time_step'] = 0.0
+
+            # Wait for thread to end
+            self.Thread.quit()
+            self.Thread.wait()
+
+            # Send stop commands to all channels
+            self.abortCMD()
+
+            # Clean before ending
+            self.dispatcher_ctrl = None
+            self.route = None
+            
+    def abortCMD(self):
+        for i in range(1,5):
+            channel = i*2
+            self.comms.write(ABORT_CMD.format(channel=channel).encode('utf_8'))
 
 
 
--- gui/dispatcher.py
+++ gui/dispatcher.py
@@ -10,37 +10,44 @@ WRITE_CMD = 'AZ.{channel}P1={sp_value:.3f}\r'
 class Dispatcher(QObject):
     finished = pyqtSignal()
 
-    def __init__(self, serial_comms, channel, step_route):
+    def __init__(self, serial_comms, channel, step_route, dispatcher_ctrl):
         super().__init__()
         self.serial_comms = serial_comms
         self.channel = channel
         self.step_route = step_route
+        self.dispatcher_ctrl = dispatcher_ctrl
 
     def run(self):
         print("WOW im running")
         initial_sp = self.step_route['initial']
-        step_delay = self.step_route['time_step']
+        #step_delay = self.step_route['time_step']
 
         # Setup initial sp rate
-        self.serial_comms.write(self.setupSP(self.channel, initial_sp))        
-        time.sleep(step_delay)
+        self.toDevice(initial_sp)
+        #self.serial_comms.write(self.setupSP(self.channel, initial_sp))        
+        time.sleep(self.step_route['time_step'])
 
         # Start with the n-steps that are covered by the step_size
         next_step = initial_sp
         for step in range(self.step_route['n_steps']):
             next_step += self.step_route['step_size']
-            self.serial_comms.write(self.setupSP(self.channel, next_step))
-            time.sleep(step_delay)
+            self.toDevice(next_step)
+            #self.serial_comms.write(self.setupSP(self.channel, next_step))
+            time.sleep(self.step_route['time_step'])
         
         # If last_step is needed, send the command
         if(self.step_route['last_flag']):
             next_step += self.step_route['last_step']
+            self.toDevice(next_step)
+            #self.serial_comms.write(self.setupSP(self.channel, next_step))
+
+
+    def toDevice(self, next_step):
+        if (self.dispatcher_ctrl['allowed']):
             self.serial_comms.write(self.setupSP(self.channel, next_step))
-
-
-        
+        else:
+            return
 
     def setupSP(self,channel, sp_value):
         cmd = WRITE_CMD.format(channel=channel, sp_value=sp_value)
-        print(cmd)
         return cmd.encode('utf_8')
